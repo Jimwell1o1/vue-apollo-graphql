1dcf8bb9b96797bd81de34f89ba9ec38b4132693
Jimwell Rabino <82427374+Jimwell1o1@users.noreply.github.com>
2022-12-08 00:39:41 +0800

    Make header and footer globally

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,7 @@
 <template>
-  <header class="flex flex-col">
-      <HeaderTop />
-  
-      <div class="flex items-end justify-center py-3">
-          <img class="w-20" src="../assets/images/apollo-logo.png" alt="">
-          <h1 class="text-4xl font-semibold text-gray-700"><span class="text-green-400">APOLLO</span> LIGHTINGS</h1>
-      </div>
-  
-      <HeaderNav />
-  </header>
-
-  <HeroItem />
-  <LightSection />
-  <ContactItem />
-  <FooterItem />
+  <hero-section />
+  <light-section />
+  <contact-section />
 </template>
 
 <script lang="ts" setup>
@@ -21,12 +9,10 @@ import { Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 // Components
-import HeaderNav from '@/components/Header/HeaderNav.vue';
-import HeaderTop from '@/components/Header/HeaderTop.vue';
-import HeroItem from '@/components/Hero/HeroItem.vue';
+import HeroSection from '@/components/Hero/HeroSection.vue';
 import LightSection from '@/components/Lights/LightSection.vue';
-import ContactItem from '@/components/Contact/ContactItem.vue';
-import FooterItem from '@/components/Footer/FooterItem.vue';
+import ContactSection from '@/components/Contact/ContactSection.vue';
+
 
 class HomeView extends Vue {}
 </script>
