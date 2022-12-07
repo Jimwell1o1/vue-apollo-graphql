6ce975240a7b8c95bb818969712ecfe80c02e557
Jimwell Rabino <82427374+Jimwell1o1@users.noreply.github.com>
2022-12-08 00:39:23 +0800

    Rename and add Router links

--- src/components/Header/HeaderNav.vue
+++ src/components/Header/HeaderNav.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="px-64 py-5">
         <ul class="flex justify-center items-center gap-12 cursor-pointer">
-            <li class="hover:text-green-400">ALL</li>
+            <li class="hover:text-green-400"><router-link to="/">ALL</router-link></li>
             <li class="hover:text-green-400">NEW IN</li>
             <li class="hover:text-green-400">BEST</li>
             <li class="hover:text-green-400">BULBS</li>
@@ -9,7 +9,7 @@
             <li class="hover:text-green-400">CHANDELIERS</li>
             <li class="hover:text-green-400">LAMPS</li>
             <li class="hover:text-green-400">SALE</li>
-            <li class="hover:text-green-400">CONTACT</li>
+            <li class="hover:text-green-400"><router-link to="/contact-list">CONTACT</router-link></li>
         </ul>
     </nav>
 </template>
