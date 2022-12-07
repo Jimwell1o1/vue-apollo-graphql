a64532d67c05896651e09afb82455b1200c75bc3
Jimwell Rabino <82427374+Jimwell1o1@users.noreply.github.com>
2022-12-08 00:41:26 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,23 @@
 <template>
-    <!-- <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      {{ result }}
-  
-      <div v-if="loading">Loading...</div>
-  
-      <div v-else-if="error">Error: {{ error.message }}</div>
-    
-      <ul v-else-if="(result && result.books)">
-        <li v-for="book of result.books" :key="book.title" class="books">
-          <p>{{ book.author }}</p> 
-          <h5>{{ book.title }}</h5>
-        </li>
-      </ul>
-  
-    </nav> -->
-    <router-view/>
+  <header class="flex flex-col">
+        <HeaderTop />
+
+        <div class="flex items-end justify-center py-3">
+            <img class="w-20" src="./assets/images/apollo-logo.png" alt="">
+            <h1 class="text-4xl font-semibold text-gray-700"><span class="text-green-400">APOLLO</span> LIGHTINGS</h1>
+        </div>
+
+        <HeaderNav />
+  </header> 
+
+  <router-view/>
+
+  <FooterItem />
   </template>
   
-  <script lang="ts">
-  
-    import { useQuery } from '@vue/apollo-composable'
-    // import booksQuery  from './graphql/books.query.gql'
-  
-    export default {
-      setup () {
-        // const { result } = useQuery(booksQuery)
-        // const books = useResult(result, null, data => data.books)
-  
-        // const { result, loading, error } = useQuery(booksQuery)
-  
-        // return {
-        //       result,
-        //       loading,
-        //       error,
-        //     }
-  
-      },
-    }
+  <script lang="ts" setup>
+    import HeaderNav from '@/components/Header/HeaderNav.vue';
+    import HeaderTop from '@/components/Header/HeaderTop.vue';
+    import FooterItem from '@/components/Footer/FooterItem.vue';
+
   </script>
